d049122e22035534517103b194ca355eea10dfc1
241024 <241024@std.hi-joho.ac.jp>
2025-05-21 10:16:22 +0900

    henkou

---
+++ src/Review/Review19.java
@@ -0,0 +1,14 @@
+package Review;
+
+public class Review19 {
+    public static void main(String[] args){
+        int num = 5;
+        int i = 1;
+        int factorial = 1;
+        while (i<=num){
+            factorial *= i;
+            i ++;
+        }
+        System.out.println("結果："+ factorial);
+    }
+}
