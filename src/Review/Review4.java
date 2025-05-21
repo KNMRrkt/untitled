7add5dc72affbd3e845160c6ff603c2151c1934a
241024 <241024@std.hi-joho.ac.jp>
2025-05-21 10:44:28 +0900

    update

---
+++ src/Review/Review4.java
@@ -0,0 +1,16 @@
+package Review;
+
+public class Review4 {
+    public static void main(String[] args){
+        int number = 5;
+        String str = 88 + "88";
+        double PI = 3.141592;
+        PI = 3.14;
+        float circ = 3.14;
+        long a = 10000000000;
+        boolean flg = true;
+        byte b = 35;
+        int num = 0x735;
+        char c = 'a';
+    }
+}
