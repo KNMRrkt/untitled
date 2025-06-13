fa71e6e21efad2cb9ffc348c44b340e7aa6e10c9
241024 <241024@std.hi-joho.ac.jp>
2025-06-13 11:40:05 +0900

    update

--- src/textbook_practice/chapter3/Practice3.java
+++ src/textbook_practice/chapter3/Practice3.java
@@ -1,5 +1,16 @@
 package textbook_practice.chapter3;
 
 public class Practice3 {
-
+    public static void main(String[] args) {
+        int isHungry = 1;
+        String food = "ラーメン";
+        System.out.println("こんにちは");
+        if (isHungry == 0) {
+            System.out.println("お腹がいっぱいです");
+        } else {
+            System.out.println("はらぺこです");
+            System.out.println(food + "をいただきます");
+            System.out.println("ごちそうさまでした");
+        }
+    }
 }
---
+++ src/textbook_practice/chapter3/Practice4.java
@@ -0,0 +1,7 @@
+package textbook_practice.chapter3;
+
+public class Practice4 {
+    public static void main(String[] args){
+        
+    }
+}
